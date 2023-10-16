ec39695d6b3d1d9547d48ce15b166c79f84965d8
Jomar Rodriguez <jomar.rodriguez_7@hotmail.com>
2023-10-16 13:27:33 -0400

    Add dist

---
+++ dist/cjs/components/Stepper.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../styles/stepper.css';
+import { StepperProps } from '../types/Stepper.types';
+declare const Stepper: (props: StepperProps) => React.JSX.Element;
+export default Stepper;
